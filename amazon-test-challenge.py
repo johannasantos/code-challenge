9d6982b7a6e2ca4533c20b0225dda32ab795f178
Johana Santos <jsantos@litebox.ai>
2019-11-17 20:00:06 -0300

    function for starting and searching Alexa

--- amazon-test-challenge.py
+++ amazon-test-challenge.py
@@ -1,5 +1,22 @@
 from selenium import webdriver
 import time
 
+def test_amazon_search():
+    driver = webdriver.Chrome()
+    try:
+        # Start Amazon
+        driver.get('http://www.amazon.com')
+        time.sleep(5)
+        # Search 'Alexa'
+        searcher = driver.find_element_by_id('twotabsearchtextbox')
+        searcher.send_keys('Alexa')
+        searcher.submit()
 
+
+
+
+
+
+    finally:
+        driver.quit()
 
