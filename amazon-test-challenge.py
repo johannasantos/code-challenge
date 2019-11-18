17cab45026a6384ad48e225a069aaf111264dd18
Johana Santos <jsantos@litebox.ai>
2019-11-17 21:36:59 -0300

    click on the third item

--- amazon-test-challenge.py
+++ amazon-test-challenge.py
@@ -12,12 +12,19 @@ def test_amazon_search():
         searcher.send_keys('Alexa')
         searcher.submit()
 
-        # Go to the second page 
+        # Navigate to the second page 
         time.sleep(5)
         paginator = driver.find_element_by_class_name('a-pagination')
         second_page = paginator.find_element_by_xpath('//li[3]/a')
         second_page.click()
 
+        # Select the third item
+        time.sleep(5)
+        item = driver.find_elements_by_class_name('s-result-item')[2]
+        item_link = item.find_element_by_class_name('a-link-normal')
+        item_link.click()
+
+        
 
 
 
