534be956615ee96051e34e934ebdd8e1f999b0fb
Johana Santos <jsantos@litebox.ai>
2019-11-17 21:28:10 -0300

    second page navigation

--- amazon-test-challenge.py
+++ amazon-test-challenge.py
@@ -12,6 +12,14 @@ def test_amazon_search():
         searcher.send_keys('Alexa')
         searcher.submit()
 
+        # Go to the second page 
+        time.sleep(5)
+        paginator = driver.find_element_by_class_name('a-pagination')
+        second_page = paginator.find_element_by_xpath('//li[3]/a')
+        second_page.click()
+
+
+
 
 
 
