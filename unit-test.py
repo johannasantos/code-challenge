cb327d85060d4b13d4decb4124abf5884d883d48
Johanna Santos <Roldan@MacBook-Pro-de-Jo-Bleu.local>
2019-12-28 09:12:11 -0300

    data intro al uso de datos

--- unit-test.py
+++ unit-test.py
@@ -8,6 +8,8 @@ from Pages.PageRegister import *
 import sys
 import xmlrunner
 import HtmlTestRunner
+from helpers.data import *
+
 
 class newTours(unittest.TestCase):
     def setUp(self):
@@ -17,21 +19,19 @@ class newTours(unittest.TestCase):
         self.page_flight = FlightPage(self.driver)
         self.page_register = PageRegister(self.driver)
 
-    @unittest.skip("No quiero que se corra el test de registro")
     def test_dropdown(self):
+        my_data = test_data()
         self.page_index.click_register()
         self.page_flight.select_country_by_index(5)
         self.page_flight.select_country_by_value('11')
-        self.page_flight.select_country_by_name('CONGO')
+        self.page_flight.select_country_by_name(my_data.country)
         self.page_flight.verify_country('CONGO')
         self.page_flight.verify_not_country('ITALY')
 
-    @unittest.skipIf(2>1, "quiero que 1 sea mas que 2")
     def test_register(self):
         self.page_index.login('test', 'test')
         self.page_register.verify_registration_form()
 
-    @unittest.skipUnless(sys.platform.startswith("linux"), "solo es para linux")
     def test_login_by_tabs(self):
         self.page_index.login_by_tab('test', 'test')
 
