ab5902486b95bf613c5ecdee8c9ac0aced30e9f5
Johanna Santos <Roldan@MacBook-Pro-de-Jo-Bleu.local>
2019-12-27 23:57:10 -0300

    ignorar test cases con varios tipos

--- unit-test.py
+++ unit-test.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from Pages.PageIndex import *
 from Pages.FlightPage import *
 from Pages.PageRegister import *
+import sys
 import xmlrunner
 import HtmlTestRunner
 
@@ -16,6 +17,7 @@ class newTours(unittest.TestCase):
         self.page_flight = FlightPage(self.driver)
         self.page_register = PageRegister(self.driver)
 
+    @unittest.skip("No quiero que se corra el test de registro")
     def test_dropdown(self):
         self.page_index.click_register()
         self.page_flight.select_country_by_index(5)
@@ -24,10 +26,12 @@ class newTours(unittest.TestCase):
         self.page_flight.verify_country('CONGO')
         self.page_flight.verify_not_country('ITALY')
 
+    @unittest.skipIf(2>1, "quiero que 1 sea mas que 2")
     def test_register(self):
         self.page_index.login('test', 'test')
         self.page_register.verify_registration_form()
 
+    @unittest.skipUnless(sys.platform.startswith("linux"), "solo es para linux")
     def test_login_by_tabs(self):
         self.page_index.login_by_tab('test', 'test')
 
